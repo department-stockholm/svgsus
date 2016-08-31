54f5ad012b94804e1bb9acc11f4b1aad5dacb74a
Fredrik Jonsson <fredrik@department.se>
2016-08-31 17:37:34 +0200

    added convert all to uibezierpath as well + tests

--- test/cleaned-svg.js
+++ test/cleaned-svg.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import {readFileSync} from 'fs';
 import {resolve} from 'path';
-import {convert} from '../lib//cleaned-svg';
+import {convert, convertAll} from '../lib//cleaned-svg';
 
 test('logo.cleaned-compressed.svg', t => {
   const original = readFileSync(resolve(__dirname, 'fixtures', 'logo.original.svg'), 'utf8');
@@ -22,6 +22,27 @@ test('logo.cleaned-4-uncompressed.svg', t => {
   const expected = readFileSync(resolve(__dirname, 'fixtures', t.title), 'utf8');
   return convert(original, {codeIndent: '    ', compressed: false})
     .then(actual => t.is(actual, expected));
+})
+test('coin.cleaned-4-uncompressed.svg', t => {
+  const original = readFileSync(resolve(__dirname, 'fixtures', 'coin.original.svg'), 'utf8');
+  const expected = readFileSync(resolve(__dirname, 'fixtures', t.title), 'utf8');
+  return convert(original, {codeIndent: '    ', compressed: false})
+    .then(actual => t.is(actual, expected));
+})
+
+test('all.cleaned-4-uncompressed.svg', t => {
+  const original = [{
+    name: 'logo.cleaned-4-uncompressed',
+    svg: readFileSync(resolve(__dirname, 'fixtures', 'logo.original.svg'), 'utf8')
+  },{
+    name: 'coin.cleaned-4-uncompressed',
+    svg: readFileSync(resolve(__dirname, 'fixtures', 'coin.original.svg'), 'utf8')
+  }];
+  return convertAll(original, {codeIndent: '    ', compressed: false})
+    .then(actual =>  Promise.all(actual.map(actual => {
+        const expected = readFileSync(resolve(__dirname, 'fixtures', actual.name), 'utf8');
+        return t.is(actual.code, expected);
+    })));
 })
 
 test(t => {
