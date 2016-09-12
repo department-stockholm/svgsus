bebf6c022d7ddc32aa3c37b46e63bcada6baa138
Fredrik Jonsson <fredrik@department.se>
2016-09-12 12:24:14 +0200

    Added svg symbol format + related tests

---
+++ test/svg-symbol.js
@@ -0,0 +1,49 @@
+import test from 'ava';
+import {readFileSync, writeFileSync} from 'fs';
+import {resolve} from 'path';
+import {convert, convertAll} from '../lib/svg-symbol';
+
+test('logo.2-symbol.svg', t => {
+  const original = readFileSync(resolve(__dirname, 'fixtures', 'logo.original.svg'), 'utf8');
+  const expected = readFileSync(resolve(__dirname, 'fixtures', t.title), 'utf8');
+  return convert(original, {codeIndent: '  '})
+    .then(actual =>  t.is(actual, expected));
+})
+test('logo.4-symbol.svg', t => {
+  const original = readFileSync(resolve(__dirname, 'fixtures', 'logo.original.svg'), 'utf8');
+  const expected = readFileSync(resolve(__dirname, 'fixtures', t.title), 'utf8');
+  return convert(original, {codeIndent: '    '})
+    .then(actual =>  t.is(actual, expected));
+})
+test('coin.2-symbol.svg', t => {
+  const original = readFileSync(resolve(__dirname, 'fixtures', 'coin.original.svg'), 'utf8');
+  const expected = readFileSync(resolve(__dirname, 'fixtures', t.title), 'utf8');
+  return convert(original, {codeIndent: '  '})
+    .then(actual =>  t.is(actual, expected));
+})
+test('coin.4-symbol.svg', t => {
+  const original = readFileSync(resolve(__dirname, 'fixtures', 'coin.original.svg'), 'utf8');
+  const expected = readFileSync(resolve(__dirname, 'fixtures', t.title), 'utf8');
+  return convert(original, {codeIndent: '    '})
+    .then(actual =>  t.is(actual, expected));
+})
+test.only('all.4-symbol.svg', t => {
+  const original = [{
+    name: 'logo.4-symbol',
+    svg: readFileSync(resolve(__dirname, 'fixtures', 'logo.original.svg'), 'utf8')
+  },{
+    name: 'coin.4-symbol',
+    svg: readFileSync(resolve(__dirname, 'fixtures', 'coin.original.svg'), 'utf8')
+  }];
+  return convertAll(original, {codeIndent: '    '})
+    .then(actual =>  {
+      writeFileSync(resolve(__dirname, 'fixtures', t.title), actual[0].code);
+      const expected = readFileSync(resolve(__dirname, 'fixtures', t.title), 'utf8');
+      t.is(actual[0].code, expected);
+    });
+})
+test(t => {
+  t.throws(() => convert(''), /codeIndent must be a string/);
+  t.throws(() => convert('', {codeIndent : '😱'}), /codeIndent must be whitespace only/);
+  t.throws(() => convert('', {codeIndent : '', name : 32}), /name must be a string/);
+})
